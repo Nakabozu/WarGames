ae2cab0715c18f3de2769b3ba7afe0a7fda56402
Nakabozu <spamnaka+github@gmail.com>
2020-06-06 22:30:11 -0500

    Start of Card class

---
+++ War.cs
@@ -0,0 +1,24 @@
+class War
+{
+    Random random = new Random();
+
+    // Main method 
+    static void Main()
+    {
+        
+
+
+
+        
+    }
+}
+
+
+
+class Card
+{
+    private string suit { get; }
+    private int value { get; }
+
+
+}

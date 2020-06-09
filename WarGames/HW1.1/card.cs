5938fa86852a2b9c622281c9e26de8abb53fba3c
Yes <66283779+sariau-bot@users.noreply.github.com>
2020-06-09 19:48:15 -0400

    here is the second attempt
    not very pretty and very bare

---
+++ WarGames/HW1.1/card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace HW1._1
+{
+    class Card
+    {
+        public int _rank;
+        public static string[] Suit = new string[] {"S", "H", "D", "C"};
+        public string _suit;
+        public Card(int rank, string suit)
+        {
+            _rank = rank;
+            _suit = suit;
+
+        }
+                public int Compare(Card c)
+        {
+            int otherCardValue = c._rank;
+            if (this._rank < otherCardValue)
+            {
+                return -1;
+            }
+            else if (this._rank > otherCardValue)
+            {
+                return 1;
+            }
+            else 
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ WarGames/HW1.1/deck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1._1
+{
+    class Deck
+    {
+        public static Card[] deck = new Card[52];
+        public static void FillDeck()
+        {
+            int index = 0;
+            foreach (string _suit in Card.Suit)
+            {
+                for(int _rank = 1; _rank <14; _rank++)
+                {
+                    Card card = new Card(_rank, _suit);
+                    deck[index] = card;
+                    index++;
+                }
+            }
+            return Shuffle(Card);
+        }
+    }
+}
